0544242027fa1816720ddd2864c0d4fe097e026e
Anthony David <anthony.david34@sfr.fr>
2024-08-16 17:41:22 +0200

    board list

---
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils";
+import { Star } from "lucide-react";
+
+interface FooterProps {
+  title: string;
+  authorLabel: string;
+  createdAtLabel: string;
+  isFavorite: boolean;
+  onClick: () => void;
+  disabled: boolean;
+}
+
+export default function Footer({
+  title,
+  authorLabel,
+  createdAtLabel,
+  isFavorite,
+  onClick,
+  disabled,
+}: FooterProps) {
+  return (
+    <div className="relative bg-white p-3">
+      <p className="text-[13px] truncate max-w-[100%-20px]">{title}</p>
+      <p className="opacity-0 group-hover:opacity-100 text-muted-foreground text-[11px] truncate">
+        {authorLabel}, {createdAtLabel}
+      </p>
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className={cn(
+          "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
+          disabled && "cursor-not-allowed opacity-75"
+        )}
+      >
+        <Star
+          className={cn("size-4", isFavorite && "fill-blue-600 text-blue-600")}
+        />
+      </button>
+    </div>
+  );
+}
