41d90d24fbad72d84309ed77e5ff27f55d330a94
Anthony David <anthony.david34@sfr.fr>
2024-08-30 16:25:29 +0200

    Room info

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -15,7 +15,7 @@ export default function Toolbar() {
   );
 }
 
-Toolbar.Skeleton = function InfoSkeleton() {
+export const ToolbarSkeleton = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
   );
